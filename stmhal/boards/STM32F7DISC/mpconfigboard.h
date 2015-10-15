d4caf54d2e6300390fe3144ebec8264928eacb9d
Fabian Riechsteiner <fabian@riechsteiner.com>
2015-10-15 16:47:50 +0200

    Added sd-card support for the STM32F7DISCO

--- stmhal/boards/STM32F7DISC/mpconfigboard.h
+++ stmhal/boards/STM32F7DISC/mpconfigboard.h
@@ -90,3 +90,8 @@ void STM32F7DISC_board_early_init(void);
 
 /*#define MICROPY_HW_USB_VBUS_DETECT_PIN (pin_J12)*/
 #define MICROPY_HW_USB_OTG_ID_PIN      (pin_A10)
+
+// SD card detect switch
+#define MICROPY_HW_SDCARD_DETECT_PIN        (pin_C13)
+#define MICROPY_HW_SDCARD_DETECT_PULL       (GPIO_PULLUP)
+#define MICROPY_HW_SDCARD_DETECT_PRESENT    (GPIO_PIN_RESET)
